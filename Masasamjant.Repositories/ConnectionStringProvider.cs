4238253f08703a8182ae7b11503767dc18876d3a
Matti Jantti <m_jantti@hotmail.com>
2025-05-14 10:15:52 +0300

    Implement simple connection string provider

---
+++ Masasamjant.Repositories/ConnectionStringProvider.cs
@@ -0,0 +1,34 @@
+﻿using Masasamjant.Repositories.Abstractions;
+
+namespace Masasamjant.Repositories
+{
+    /// <summary>
+    /// Represents connection string provider of specified value.
+    /// </summary>
+    public sealed class ConnectionStringProvider : IConnectionStringProvider
+    {
+        private readonly string connectionString;
+
+        /// <summary>
+        /// Initializes new instance of the <see cref="ConnectionStringProvider"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string to provide.</param>
+        /// <exception cref="ArgumentException">If <paramref name="connectionString"/> is empty or only white-space.</exception>
+        public ConnectionStringProvider(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("The value cannot be empty or only white-space.", nameof(connectionString));
+
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Gets the connection string.
+        /// </summary>
+        /// <returns>A connection string.</returns>
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}
